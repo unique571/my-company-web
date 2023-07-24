a96e7f219c416dcd84cd9a781249f92a5605cc95
uniaue <115631356+unique571@users.noreply.github.com>
2023-07-24 17:49:26 +0100

    completed
    go to go

---
+++ js/main.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', () =>{
+
+    const preloader = document.querySelector('.js-preloader');
+    preloader.classList.add('fade-out');
+
+setTimeout(() =>{
+    preloader.style.display='none';
+    // animate on scroll
+    AOS.init();
+}, 1000)
+})
+
+
+
+// header bg reveal
+const headerBg = () =>{
+const header = document.querySelector(".js-header");
+window.addEventListener('scroll', function() {
+    if(this.scrollY > 0) {
+        header.classList.add('bg-reveal')
+    }else{
+        header.classList.remove('bg-reveal')
+    }
+})
+}
+headerBg();
+
+
+// nav 
+
+
+const navigation = () =>{
+const navToggler = document.querySelector(".js-nav-toggler");
+const nav = document.querySelector(".js-nav");
+const navItems =nav.querySelectorAll("li");
+
+const navToggle = () =>{
+    nav.classList.toggle("open");
+    navToggler.classList.toggle("active");
+}
+navToggler.addEventListener("click", navToggle);
+
+navItems.forEach((li) => {
+    li.querySelector('a').addEventListener('click', () =>{
+        if(window.innerWidth <= 767){
+            navToggle();
+        }
+    })
+})
+}
+navigation();
---
+++ js/style-switcher.js
@@ -0,0 +1,82 @@
+
+// style switcher
+
+const styleSwitcherToggle = () =>{
+    const styleSwitcher = document.querySelector(".js-style-switcher");
+    const styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
+
+    styleSwitcherToggler.addEventListener("click", function() {
+        styleSwitcher.classList.toggle("open")
+
+        this.querySelector("i").classList.toggle("fa-times");
+        this.querySelector("i").classList.toggle("fa-cog");
+
+    })
+}
+styleSwitcherToggle();
+
+
+const themeColor = () =>{
+    const hueSlider = document.querySelector(".js-hue-slider");
+    const html = document.querySelector("html");
+
+    const setHue = (value) =>{
+        html.style.setProperty("--hue", value);
+        document.querySelector('.js-hue').innerHTML = value;
+    }
+    hueSlider.addEventListener("input", function() {
+        setHue(this.value);
+
+        localStorage.setItem("--hue", this.value);
+    });
+
+    // check for saved user
+    const slider = (value) =>{
+       hueSlider.value = value;
+    }
+
+    if(localStorage.getItem("--hue") !== null){
+        setHue(localStorage.getItem("--hue"));
+        slider(localStorage.getItem("--hue"));
+    }
+    else{
+        const hue = getComputedStyle(html).getPropertyValue("--hue");
+        setHue(hue);
+        slider(hue.split(" ").join(" "));
+    }
+}
+themeColor();
+
+
+/*    theme light & dark mode*/ 
+
+const themeLightDark = () =>{
+const darkModeCheckbox = document.querySelector(".js-dark-mode");
+
+const themeMode = () =>{
+    if(localStorage.getItem("theme-dark") === "false"){
+document.body.classList.remove("t-dark")
+    }
+    else{
+        document.body.classList.add("t-dark")
+
+    }
+}
+
+darkModeCheckbox.addEventListener("click", function(){
+//    set the user's preference in the local storage
+
+localStorage.setItem("theme-dark", this.checked);
+themeMode();
+})
+// check for saved preference, if any , on load the website
+
+if(localStorage.getItem("theme-dark") !== null){
+themeMode();
+}
+if(document.body.classList.contains("t-dark")){
+darkModeCheckbox.checked = true;
+}
+}
+
+themeLightDark();
